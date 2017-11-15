be68aebc5a7919023a5dcb8a55207e2e0cc36add
Yiren Wang <ywang@evenium.us>
2017-11-15 13:00:50 -0800

    coloooors

--- colours.py
+++ colours.py
@@ -1,4 +1,16 @@
 class Colours:
     def __init__(self):
-        colours = ["yellow", "green1", "green2", "pink", "blue", "red", "orange"] #brightest to darkest TODO with this chart: http://www.december.com/html/spec/colorhslhex10.html
-        saturations = {} # {colour: []}
+        colours = ["yellow", "green1", "green2", "blue1", "blue2", "red", "orange", "purple", "pink1", "pink2", "random faded colours"] #brightest to darkest TODO with this chart: http://www.december.com/html/spec/colorhslhex10.html
+        saturations = {
+            "yellow"    : [], 
+            "green1"    : [], 
+            "green2"    : [], 
+            "blue1"     : [], 
+            "blue2"     : [], 
+            "red"       : [], 
+            "orange"    : [], 
+            "purple"    : [], 
+            "pink1"     : [], 
+            "pink2"     : [],
+            "random faded colours"
+        } # {colour: []}
